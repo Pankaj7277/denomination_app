375e68177b9d1cdd15a328199e4b1d83edbc09b9
Pankaj7277 <pankajkr.v21@gmail.com>
2025-03-06 23:11:38 +0530

    updated

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -37,240 +37,280 @@ class _HomepageState extends State<Homepage> {
     });
     setState(() {
       totalAmount = total;
-      amountInWords = capitalizeFirstLetter(NumberToWordsEnglish.convert(totalAmount));
+      amountInWords =
+          capitalizeFirstLetter(NumberToWordsEnglish.convert(totalAmount));
     });
   }
 
   String capitalizeFirstLetter(String input) {
-  if (input.isEmpty) return input;
-  return input[0].toUpperCase() + input.substring(1).toLowerCase();
-}
-void saveDataToStorage() {
-  showDialog(
-    context: context,
-    builder: (context) {
-      String remarks = "";
-      String selectedOption = "General"; // Default dropdown value
-      return AlertDialog(
-         backgroundColor:  const Color.fromARGB(255, 17, 23, 27),
-        title: const Center(child: Text("Save Data", style: TextStyle(color: Colors.white),)),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-           
-            DropdownButtonFormField<String>(
-              dropdownColor: Colors.black,
-              value: selectedOption,
-              items: ["General", "Income", "Expense"]
-                  .map((option) => DropdownMenuItem<String>(
-                        value: option,
-                        child: Text(option, style: TextStyle(color: Colors.white),),
-                      ))
-                  .toList(),
-              decoration: const InputDecoration(
-                labelText: "Category",
-                border: OutlineInputBorder(),
+    if (input.isEmpty) return input;
+    return input[0].toUpperCase() + input.substring(1).toLowerCase();
+  }
+
+  void saveDataToStorage() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        String remarks = "";
+        String selectedOption = "General"; // Default dropdown value
+        return AlertDialog(
+          backgroundColor: const Color.fromARGB(255, 17, 23, 27),
+          title: const Center(
+              child: Text(
+            "Save Data",
+            style: TextStyle(color: Colors.white),
+          )),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              DropdownButtonFormField<String>(
+                dropdownColor: Colors.black,
+                value: selectedOption,
+                items: ["General", "Income", "Expense"]
+                    .map((option) => DropdownMenuItem<String>(
+                          value: option,
+                          child: Text(
+                            option,
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ))
+                    .toList(),
+                decoration: const InputDecoration(
+                  labelText: "Category",
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (value) {
+                  if (value != null) {
+                    selectedOption = value;
+                  }
+                },
               ),
-              onChanged: (value) {
-                if (value != null) {
-                  selectedOption = value;
+              const SizedBox(height: 16),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: "Fill your remark(if any)",
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (value) {
+                  remarks = value;
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: const Text("Cancel"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                try {
+                  final List<Map<String, dynamic>> storedData = storage
+                          .read<List<dynamic>>('denomination_history')
+                          ?.cast<Map<String, dynamic>>() ??
+                      [];
+                  final newData = {
+                    'counts': counts,
+                    'totalAmount': totalAmount,
+                    'remarks': remarks,
+                    'category': selectedOption,
+                    'timestamp': DateTime.now().toIso8601String(),
+                  };
+                  storedData.add(newData);
+                  storage.write('denomination_history', storedData);
+                  print("Data saved successfully: $newData");
+                  setState(() {
+                    counts.updateAll((key, value) => 0);
+                    controllers
+                        .forEach((key, controller) => controller.clear());
+                    totalAmount = 0;
+                  });
+                  Navigator.pop(context); // Close the dialog
+                } catch (e) {
+                  print("Error saving data: $e");
                 }
               },
+              child: const Text("Save"),
             ),
-            const SizedBox(height: 16),
-             TextField(
-              decoration: const InputDecoration(
-                labelText: "Fill your remark(if any)",
-                border: OutlineInputBorder(),
-              ),
-              onChanged: (value) {
-                remarks = value;
-              },
-            ),
-            
           ],
-        ),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.pop(context); // Close the dialog
-            },
-            child: const Text("Cancel"),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              try {
-                final List<Map<String, dynamic>> storedData =
-                    storage.read<List<dynamic>>('denomination_history')?.cast<Map<String, dynamic>>() ?? [];
-                final newData = {
-                  'counts': counts,
-                  'totalAmount': totalAmount,
-                  'remarks': remarks,
-                  'category': selectedOption,
-                  'timestamp': DateTime.now().toIso8601String(),
-                };
-                storedData.add(newData);
-                storage.write('denomination_history', storedData);
-                print("Data saved successfully: $newData");
-                setState(() {
-                  counts.updateAll((key, value) => 0);
-                  controllers.forEach((key, controller) => controller.clear());
-                  totalAmount = 0;
-                });
-                Navigator.pop(context); // Close the dialog
-              } catch (e) {
-                print("Error saving data: $e");
-              }
-            },
-            child: const Text("Save"),
-          ),
-        ],
-      );
-    },
-  );
-}
-
-
-
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Container(
-                height: 200,
-              width: MediaQuery.of(context).size.width,
-              decoration: const BoxDecoration(
-                image: DecorationImage(image: AssetImage("assets/images/currency_banner.jpg"),fit: BoxFit.cover)),
-                child:  Align(
-                  alignment: Alignment.bottomLeft,
-                  child: Padding(
-                    padding:  const EdgeInsets.all(8.0),
-                    child: totalAmount>0 ? Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      crossAxisAlignment: CrossAxisAlignment.start,
+        body: Column(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image:
+                              AssetImage("assets/images/currency_banner.jpg"),
+                          fit: BoxFit.cover)),
+                  child: Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: totalAmount > 0
+                            ? Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text("Total Amount",
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.w500)),
+                                  Text("₹ $totalAmount",
+                                      style: const TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w500)),
+                                  Text("$amountInWords only/-",
+                                      style: const TextStyle(
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w400)),
+                                ],
+                              )
+                            : const Text(
+                                "Denomination",
+                                style: TextStyle(fontSize: 28),
+                              ),
+                      )),
+                ),
+                Positioned(
+                    top: 25,
+                    right: 10,
+                    child: IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => History()));
+                        },
+                        icon: const Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                        )))
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: denominations.length,
+                itemBuilder: (context, index) {
+                  int denomination = denominations[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(2.0),
+                    child: Row(
                       children: [
-                       const Text("Total Amount", style: TextStyle(fontSize: 22,fontWeight: FontWeight.w500)),
-                        Text("₹ $totalAmount", style:const TextStyle(fontSize: 18,fontWeight: FontWeight.w500)),
-                        Text("$amountInWords only/-", style:const TextStyle(fontSize: 14, fontWeight: FontWeight.w400)),
-                      ],
-                    ):const Text("Denomination", style: TextStyle(fontSize: 28),),
-                  )),
-              ),
-              Positioned(
-                top: 25,
-                right: 10,
-                child: IconButton(onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=> History()));
-
-                }, icon: const Icon(Icons.more_vert, color: Colors.white,)))
-            ],
-          ),
-        
-          Expanded(
-            child: ListView.builder(
-              itemCount: denominations.length,
-              itemBuilder: (context, index) {
-                int denomination = denominations[index];
-                return Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Text(
-                          "₹$denomination",
-                          style: const TextStyle(fontSize: 18),
-                        ),
-                      ),
-                      const SizedBox(width: 10),
-                      const Text("x", style: TextStyle(fontSize: 18)),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        flex: 4,
-                        child: TextField(
-                          controller: controllers[denomination],
-                          keyboardType: TextInputType.number,
-                         inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
+                        Expanded(
+                          flex: 2,
+                          child: Text(
+                            "₹$denomination",
+                            style: const TextStyle(fontSize: 18),
                           ),
-                          onChanged: (value) {
-                            setState(() {
-                              counts[denomination] =
-                                  int.tryParse(value) ?? 0;
-                              updateTotalAmount();
-                            });
-                          },
                         ),
-                      ),
-                      const SizedBox(width: 10),
-                      Expanded(
-                        flex:2,
-                        child: Text(
-                          "= ₹${counts[denomination]! * denomination}",
-                          style:const TextStyle(fontSize: 18),
+                        const SizedBox(width: 10),
+                        const Text("x", style: TextStyle(fontSize: 18)),
+                        const SizedBox(width: 10),
+                        Expanded(
+                          flex: 4,
+                          child: TextField(
+                            controller: controllers[denomination],
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly
+                            ],
+                            decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                            ),
+                            onChanged: (value) {
+                              setState(() {
+                                counts[denomination] = int.tryParse(value) ?? 0;
+                                updateTotalAmount();
+                              });
+                            },
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
-      ),
-       floatingActionButton: totalAmount>0?Stack(
-        children: [
-          if (isExpanded)
-            Positioned(
-              bottom: 80,
-              right: 16,
-              child: FloatingActionButton(
-                tooltip: "Clear",
-                heroTag: "clear",
-                onPressed: (){
-                 setState(() {
-                    counts.updateAll((key, value) => 0);
-                  controllers.forEach((key, controller) => controller.clear());
-                  totalAmount = 0;
-                 });
+                        const SizedBox(width: 10),
+                        Expanded(
+                          flex: 2,
+                          child: Text(
+                            "= ₹${counts[denomination]! * denomination}",
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
                 },
-                backgroundColor: Colors.grey[800],
-                child:const Icon(Icons.restart_alt,color: Colors.white,),
               ),
             ),
-          if (isExpanded)
-            Positioned(
-              bottom: 140,
-              right: 16,
-              child: FloatingActionButton(
-                   tooltip: "Save",
-                heroTag: "save",
-                onPressed: saveDataToStorage,
-                backgroundColor: Colors.grey[800],
-                child:const Icon(Icons.file_download,color: Colors.white),
-              ),
-            ),
-          Positioned(
-            bottom: 16,
-            right: 16,
-            child: FloatingActionButton(
-              shape: const CircleBorder(),
-              onPressed: () {
-                setState(() {
-                  isExpanded = !isExpanded; // Toggle visibility
-                });
-              },
-              backgroundColor: Colors.blue,
-              child: const Icon(Icons.bolt, color: Colors.white,),
-            ),
-          ),
-        ],
-      ): null
-    );
+          ],
+        ),
+        floatingActionButton: totalAmount > 0
+            ? Stack(
+                children: [
+                  if (isExpanded)
+                    Positioned(
+                      bottom: 80,
+                      right: 16,
+                      child: FloatingActionButton(
+                        tooltip: "Clear",
+                        heroTag: "clear",
+                        onPressed: () {
+                          setState(() {
+                            counts.updateAll((key, value) => 0);
+                            controllers.forEach(
+                                (key, controller) => controller.clear());
+                            totalAmount = 0;
+                          });
+                        },
+                        backgroundColor: Colors.grey[800],
+                        child: const Icon(
+                          Icons.restart_alt,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  if (isExpanded)
+                    Positioned(
+                      bottom: 140,
+                      right: 16,
+                      child: FloatingActionButton(
+                        tooltip: "Save",
+                        heroTag: "save",
+                        onPressed: saveDataToStorage,
+                        backgroundColor: Colors.grey[800],
+                        child: const Icon(Icons.file_download,
+                            color: Colors.white),
+                      ),
+                    ),
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: FloatingActionButton(
+                      shape: const CircleBorder(),
+                      onPressed: () {
+                        setState(() {
+                          isExpanded = !isExpanded; // Toggle visibility
+                        });
+                      },
+                      backgroundColor: Colors.blue,
+                      child: const Icon(
+                        Icons.bolt,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : null);
   }
 }
